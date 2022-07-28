52e22949cd0c010b288179086380d16cf7ac25a6
Sam <sam.seabourn@gmail.com>
2022-07-29 07:06:45 +1000

    chore(launcher): add check for special characters

--- KittenPingPong.js
+++ KittenPingPong.js
@@ -1,44 +1,44 @@
 const SECRETS = require("./secrets.js");
 const tmi = require("tmi.js");
 const axios = require("axios");
-const express = require('express')
-const app = express()
-const port = 3000
-const open = require('open');
+const express = require("express");
+const app = express();
+const port = 3000;
+const open = require("open");
 
 let ballCount = 25;
 let ballsToBeDropped = 0; //How many are is que
-let isHarambe = false
+let isHarambe = false;
 
 ////Express server junk
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.post('/currentBallCount', (req, res) => {
+app.post("/currentBallCount", (req, res) => {
   res.json({
     ballCount,
     ballsToBeDropped,
-    isHarambe
-  })
-})
+    isHarambe,
+  });
+});
 
-app.post('/dropball',(req, res) => {
+app.post("/dropball", (req, res) => {
   if (ballsToBeDropped < 0) ballsToBeDropped = 0;
-  ballsToBeDropped--
-  ballCount --
-  isHarambe = false
-  launchBall()
-  res.json({})
-})
+  ballsToBeDropped--;
+  ballCount--;
+  isHarambe = false;
+  launchBall();
+  res.json({});
+});
 
 app.listen(port, () => {
-  console.log(`Ball counter listening at http://localhost:${port}`)
-})
+  console.log(`Ball counter listening at http://localhost:${port}`);
+});
 
-open('http://localhost:3000/index.html');
+open("http://localhost:3000/index.html");
 
 const client = new tmi.Client({
   options: { debug: true },
@@ -70,65 +70,108 @@ const launchBall = () => {
 };
 
 const resetBallCount = () => {
-  ballCount = 25
-}
+  ballCount = 25;
+};
 
-client.on("message", (channel, tags, message, self) => {
-  if (self) return;
-  //Ignores itself if accidentally uses a trigger ir someone is logged into the account.
+client.on(
+  "message",
+  (channel, tags, message, self) => {
+    if (self) return;
+    //Ignores itself if accidentally uses a trigger ir someone is logged into the account.
 
-  if (message === "!ballcount" || message === "!bc") {
-    client.say(channel, `@${tags.username} there is ${ballCount} ball${ballCount !== 1 ? "s" : ""} remaining`);
-  }
-  //Returns the remaining amount of balls in the tube - Avalible to everyone.
-
-  const userName  = tags.username.toLowerCase();
-  //Grabs persons twitch user and makes it into a const for the triggers to pull into messages.
-
-  if ((userName === "mawrtron" || userName === "tormented_hazard" || userName === "bic_dig_boii" || userName ==="kittenclubhouse") && (message === "!fire" || message === "fire!")) {
-    ballsToBeDropped++
-    client.say(channel,`Commence drop sequence!!`);
-  }
-
-  if ((userName === "mawrtron" || userName === "tormented_hazard") && message === "test") {
-    ballsToBeDropped++
-  }
-  //Allows the manual firing of pingpongballs if there are bits donated when there are none in the chamber.
-
-  if (userName === "mawrtron" || userName === "tormented_hazard" || userName === "bic_dig_boii" || userName === "kittenclubhouse") {
-    if (message === "balls out for Harambe") {
-      isHarambe = true;
-      ballsToBeDropped = 2
-      client.say(channel,`Thanks @${userName}! Dropping two hairy balls for our fallen brother, Harambe! RIP hairy lad.`);
+    if (message === "!ballcount" || message === "!bc") {
+      client.say(
+        channel,
+        `@${tags.username} there is ${ballCount} ball${
+          ballCount !== 1 ? "s" : ""
+        } remaining`
+      );
     }
-  }
-  //Dont worry about this, easter eggs are fun. - Avalible to only the coolest of dudes.
+    //Returns the remaining amount of balls in the tube - Avalible to everyone.
 
-  if ((userName === "mawrtron" || userName === "tormented_hazard" || userName === "bic_dig_boii" || userName === "kittenclubhouse") && message === "!reload") {
-    resetBallCount();
-    client.say(channel, `locked and loaded!`);
-  }
-  //If balls are restocked use the !reload command in twitch chat to reset the counter. -Avalibe to admins. Set the userNames as your admins twitch users.
+    const userName = tags.username.toLowerCase();
+    //Grabs persons twitch user and makes it into a const for the triggers to pull into messages.
 
-  //This is the main triggering mechanism, when someone donates bits, stream labs announces it and this function grabs that and sends the HTTP request.  -Avalibe to StreamElements only!
-  if (userName === "streamelements" && message.includes("bitties for the kitties!")) { // Stream elements zone + custom bit donation message ender for the statement to zone in on.
-    const userNameFromString = message.split(" ")[0];
-    const bitCount = message.split(" ")[3];
-    //Grabs bit amount and user name to pass on to check if enough bits were donated.
-
-    //Makes sure there are balls to drop first.
-    if (ballCount == 0) {
-      client.say(channel, `There are no balls remaining, nag me to refill!`)
-    } else if (parseInt(bitCount) >= 300) {
-      ballsToBeDropped++
-      client.say(channel,`Thanks @${userNameFromString}! Dropping another ball for the kittens`);
-      //If the bit donation is enough it triggers a drop and thanks the user.
-    } else {
-      //Do nothing
+    if (
+      (userName === "mawrtron" ||
+        userName === "tormented_hazard" ||
+        userName === "bic_dig_boii" ||
+        userName === "kittenclubhouse" ||
+        userName === "Tenshi_Artesian" ||
+        userName === "Amouranth") &&
+      (message === "!fire" || message === "fire!")
+    ) {
+      ballsToBeDropped++;
+      client.say(channel, `Commence drop sequence!!`);
     }
 
+    if (
+      (userName === "mawrtron" ||
+        userName === "tormented_hazard" ||
+        userName === "kittenclubhouse") &&
+      message === "test"
+    ) {
+      client.say(channel, `This is not a drill!!... JK we are testing`);
+      ballsToBeDropped++;
     }
+    //Allows the manual firing of pingpongballs if there are bits donated when there are none in the chamber.
 
+    if (
+      userName === "mawrtron" ||
+      userName === "tormented_hazard" ||
+      userName === "bic_dig_boii" ||
+      userName === "kittenclubhouse" ||
+      userName === "Tenshi_Artesian"
+    ) {
+      if (message === "balls out for Harambe") {
+        isHarambe = true;
+        ballsToBeDropped = 2;
+        client.say(
+          channel,
+          `Thanks @${userName}! Dropping two hairy balls for our fallen brother, Harambe! RIP hairy lad.`
+        );
+      }
+    }
+    //Dont worry about this, easter eggs are fun. - Avalible to only the coolest of dudes.
+
+    if (
+      (userName === "mawrtron" ||
+        userName === "tormented_hazard" ||
+        userName === "bic_dig_boii" ||
+        userName === "kittenclubhouse") &&
+      message === "!reload"
+    ) {
+      resetBallCount();
+      client.say(channel, `locked and loaded!`);
+    }
+    //If balls are restocked use the !reload command in twitch chat to reset the counter. -Avalibe to admins. Set the userNames as your admins twitch users.
+
+    //This is the main triggering mechanism, when someone donates bits, stream labs announces it and this function grabs that and sends the HTTP request.  -Avalibe to StreamElements only!
+    if (
+      userName === "streamelements" &&
+      message.includes("bitties for the kitties!")
+    ) {
+      // Stream elements zone + custom bit donation message ender for the statement to zone in on.
+      // const userNameFromString = message.split(" ")[0]; //OLD kinda broken
+      // const bitCount = message.split(" ")[3]; //OLD kinda broken
+      const userNameFromString = message.split("with the")[0]; //TODO: make into a constant variable and work out better a way of checking for special character usernames
+      const bitCount = message.split("with the")[1].split(" ")[1];
+      //Grabs bit amount and user name to pass on to check if enough bits were donated.
+
+      //Makes sure there are balls to drop first.
+      if (ballCount == 0) {
+        client.say(channel, `There are no balls remaining, nag me to refill!`);
+      } else if (parseInt(bitCount) >= 300) {
+        ballsToBeDropped++;
+        client.say(
+          channel,
+          `Thanks @${userNameFromString}! Dropping another ball for the kittens`
+        );
+        //If the bit donation is enough it triggers a drop and thanks the user.
+      } else {
+        //Do nothing
+      }
+    }
   }
   // No user interaction
 );
